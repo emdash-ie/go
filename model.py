8551e6ac6fb59c434ae24ca290d73c9c13a95b0b
Noel Bourke <undergroundquizscene@gmail.com>
2018-10-22 23:17:08 +0100

    Add Store.insert() and Store.remove()
    Nested structures now have all the functionality of the flat structure,
    though error handling isn’t super detailed. It might be nicer to make
    the Store type immutable, and possibly also to return the errors rather
    than raising them, but this’ll do for now.

--- model.py
+++ model.py
@@ -10,18 +10,46 @@ class Store(object):
     def __init__(self, ps: SubStore):
         self.paths = ps
 
-    def lookup(self, ns: List[str]) -> str:
-        next_level = self.paths[ns[0]]
+    def lookup(self, key: List[str]) -> str:
+        next_level = self.paths[key[0]]
         if isinstance(next_level, str):
-            if len(ns) == 1:
+            if len(key) == 1:
                 return next_level
             else:
-                raise PathTooLongError(next_level, ns)
+                raise PathTooLongError(next_level, key)
         else:
-            if len(ns) == 1:
-                raise PathTooShortError(self.paths, ns)
+            if len(key) == 1:
+                raise PathTooShortError(self.paths, key)
             else:
-                return next_level.lookup(ns[1:])
+                return next_level.lookup(key[1:])
+
+    def insert(self, key: List[str], value: str) -> None:
+        if key == []:
+            raise PathTooShortError(self.paths, key)
+        elif len(key) == 1:
+            if key[0] in self.paths:
+                raise ExistingKey(self.paths, key)
+            else:
+                self.paths[key[0]] = value
+        else:
+            next = self.paths[key[0]]
+            if isinstance(next, str):
+                raise KeyConflictError(self.paths, key)
+            else:
+                next.insert(key[1:], value)
+
+    def remove(self, key: List[str]) -> None:
+        if key == []:
+            raise PathTooShortError(self.paths, key)
+        elif key[0] not in self.paths:
+            raise NonExistingKeyError(self.paths, key)
+        else:
+            if len(key) == 1:
+                del self.paths[key[0]]
+            elif isinstance(self.paths[key[0]], str):
+                raise PathTooLongError(self.paths, key)
+            else:
+                self.paths[key[0]].remove(key[1:])
 
     def __str__(self) -> str:
         return str(self.paths)
@@ -35,10 +63,30 @@ class PathTooLongError(StoreError):
 class PathTooShortError(StoreError):
     pass
 
+class ExistingKey(StoreError):
+    pass
+
+class NonExistingKeyError(StoreError):
+    pass
+
+class KeyConflictError(StoreError):
+    pass
+
 def test_lookup(ns: str) -> str:
-    d: Dict[str, Union[Store, str]] = {'labs': Store({'ai': '/path/to/ai/labs/',
-            'compiler': '/path/to/compiler/labs/'}),
-        'lectures': Store({'ai': '/path/to/ai/lectures/',
-            'compiler': '/path/to/compiler/lectures/'})}
-    g = Store(d)
-    return g.lookup(ns.split())
+    return test_store().lookup(ns.split())
+
+def test_insert(key: str, value: str) -> Store:
+    s = test_store()
+    s.insert(key.split(), value)
+    if s.lookup(key.split()) == value:
+        print('Lookup after insertion returns inserted value')
+    else:
+        print('Lookup after insertion does not return inserted value')
+    return s
+
+def test_store() -> Store:
+    return Store(
+        {'labs': Store(
+            {'ai': '/path/to/ai/labs/', 'compiler': '/path/to/compiler/labs/'}),
+        'lectures': Store(
+            {'ai': '/path/to/ai/lectures/', 'compiler': '/path/to/compiler/lectures/'})})
